6a5e1df60510e1a89906910fcf0b9185345a5da6
Ludwika Malinowska <malinowska.L@wp.pl>
2020-12-03 23:34:44 +0100

    CaesarCipher - Szyfrowanie - dodano test z szyfrowaniem pangramu 'the quick brown fox jumps over the lazy dog' - Py Hamcrest

--- tests/CaesarCipherTests/test_CaesarCipherPyHamcrest.py
+++ tests/CaesarCipherTests/test_CaesarCipherPyHamcrest.py
@@ -39,6 +39,10 @@ class CaesarTest(unittest.TestCase):
     def test_encode_veni_vidi_vici_equal(self):
         assert_that(self.temp.caesar_cipher("veni vidi vici"), equal_to("yhql ylgl ylfl"))
 
+    def test_encode_pangram_equal(self):
+        assert_that(self.temp.caesar_cipher("the quick brown fox jumps over the lazy dog"),
+                    equal_to("wkh txlfn eurzq ira mxpsv ryhu wkh odcb grj"))
+
     def test_decode_a_equal(self):
         assert_that(self.temp.caesar_decipher("d"), equal_to("a"))
 
