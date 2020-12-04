8626765a5324e30c115cb2baff9878e257f2fa17
Ludwika Malinowska <malinowska.L@wp.pl>
2020-12-04 17:33:09 +0100

    AffineCipher - szyfrowanie 'z' na 'c' z parametrami 1,3 - Red - unittest

--- tests/AffineCipherTests/test_AffineCipher.py
+++ tests/AffineCipherTests/test_AffineCipher.py
@@ -11,6 +11,9 @@ class CaesarTest(unittest.TestCase):
     def test_a_cipher(self):
         self.assertEqual(self.temp.affine_cipher("a", 1, 3), "d")
 
+    def test_z_cipher(self):
+        self.assertEqual(self.temp.affine_cipher("z", 1, 3), "c")
+
     def tearDown(self):
         self.temp = None
 
