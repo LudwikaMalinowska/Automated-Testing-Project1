b19af17504ac59a7b7fd2ec25930b26f66089fed
Ludwika Malinowska <malinowska.L@wp.pl>
2020-12-04 13:51:54 +0100

    CaesarCipher - deszyfrowanie 'zabc' na 'wxyz' - Testy parametryczne

--- tests/CaesarCipherTests/CaesarParameterizedPackageDecipher.py
+++ tests/CaesarCipherTests/CaesarParameterizedPackageDecipher.py
@@ -15,6 +15,7 @@ class CaesarParameterizedPackage(unittest.TestCase):
         ("a", "x"),
         ("c", "z"),
         ("def", "abc"),
+        ("zabc", "wxyz"),
     ])
     def test_one_parameterized(self, text, result):
         self.assertEqual(self.tmp.caesar_decipher(text), result)
@@ -29,6 +30,7 @@ class CaesarParameterizedPackage(unittest.TestCase):
     ("a", "x"),
     ("c", "z"),
     ("def", "abc"),
+    ("zabc", "wxyz"),
 ])
 def test_morse_encode_outside_class(text, result):
     r = CaesarCipher()
@@ -41,6 +43,7 @@ def test_morse_encode_outside_class(text, result):
     ("a", "x"),
     ("c", "z"),
     ("def", "abc"),
+    ("zabc", "wxyz"),
 ])
 class MorseCodeParameterizedPackage2(unittest.TestCase):
 
