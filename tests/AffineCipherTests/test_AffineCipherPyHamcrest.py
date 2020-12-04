5e53a9f2174b640197b1fbf32e828b2c924f2e2f
Ludwika Malinowska <malinowska.L@wp.pl>
2020-12-04 16:05:34 +0100

    AffineCipher - szyfrowanie 'a' na 'd' z parametrami 1,3 - Red - Py Hamcrest

---
+++ tests/AffineCipherTests/test_AffineCipherPyHamcrest.py
@@ -0,0 +1,20 @@
+import unittest
+from AffineCipher.AffineCipher import AffineCipher
+from hamcrest import *
+from hamcrest.core.base_matcher import BaseMatcher
+
+
+class CaesarTest(unittest.TestCase):
+
+    def setUp(self):
+        self.temp = AffineCipher()
+
+    def test_encode_a_equal(self):
+        assert_that(self.temp.affine_cipher("a", 1, 3), equal_to("d"))
+
+    def tearDown(self):
+        self.temp = None
+
+
+if __name__ == '__main__':
+    unittest.main()
