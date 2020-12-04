5772e6828bdbc04909fc55746dbd84cdcdd0236d
Ludwika Malinowska <malinowska.L@wp.pl>
2020-12-04 19:28:32 +0100

    AffineCipher - deszyfrowanie 'd' na 'a' z parametrami 1,3 - Red - Testy z pliku

--- tests/AffineCipherTests/AffineCipherParameterizedFile.py
+++ tests/AffineCipherTests/AffineCipherParameterizedFile.py
@@ -17,6 +17,19 @@ class CaesarParameterizedFile(unittest.TestCase):
                 self.assertEqual(tmpCaesar.affine_cipher(text, a, b), affine_text)
         fileTest.close()
 
+    def test_from_file_decipher(self):
+        fileTest = open("../../data/data_affine_decipher_test")
+        tmpCaesar = AffineCipher()
+        for line in fileTest:
+            if line.startswith("#") or line.startswith(" ") or line.startswith("\n"):
+                continue
+            else:
+                data = line.split(",")
+                affine_text, a, b, text = str(data[0]), int(data[1]),\
+                                          int(data[2]), str(data[3].strip("\n"))
+                self.assertEqual(tmpCaesar.affine_decipher(affine_text, a, b), text)
+        fileTest.close()
+
 
 if __name__ == '__main__':
     unittest.main()
