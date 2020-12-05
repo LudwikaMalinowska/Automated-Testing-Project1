6614e472716fd284154faadec844275bcb630b19
Ludwika Malinowska <malinowska.L@wp.pl>
2020-12-05 20:34:18 +0100

    AffineCipher - Szyfrowanie - wyjatek: parametr a musi byc typu int - Red - Py Hamcrest

--- tests/AffineCipherTests/test_AffineCipherPyHamcrest.py
+++ tests/AffineCipherTests/test_AffineCipherPyHamcrest.py
@@ -59,6 +59,9 @@ class AffineTest(unittest.TestCase):
     def test_cipher_exception5(self):
         assert_that(calling(self.temp.affine_cipher).with_args("abc", 3, -12), raises(ValueError))
 
+    def test_cipher_exception6(self):
+        assert_that(calling(self.temp.affine_cipher).with_args("abc", "3", 12), raises(ValueError))
+
     def test_decipher_exception1(self):
         assert_that(calling(self.temp.affine_decipher).with_args(1, 1, 3), raises(ValueError))
 
