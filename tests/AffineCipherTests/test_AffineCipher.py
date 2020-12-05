98295ef6b9e08fca66f1b6f6981aca2cc96ee4b4
Ludwika Malinowska <malinowska.L@wp.pl>
2020-12-05 18:25:58 +0100

    AffineCipher - Deszyfrowanie - wyjatek: parametr a musi byc dodatni - Red - unittest

--- tests/AffineCipherTests/test_AffineCipher.py
+++ tests/AffineCipherTests/test_AffineCipher.py
@@ -64,6 +64,9 @@ class CaesarTest(unittest.TestCase):
     def test_decipher_exception3(self):
         self.assertRaises(ValueError, self.temp.affine_decipher, "ą", 1, 3)
 
+    def test_decipher_exception4(self):
+        self.assertRaises(ValueError, self.temp.affine_decipher, "abc", -1, 3)
+
     def tearDown(self):
         self.temp = None
 
