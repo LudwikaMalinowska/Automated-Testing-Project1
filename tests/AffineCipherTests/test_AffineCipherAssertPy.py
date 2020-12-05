3af5761938debb361ad912e854ed9a8b2cf83ba2
Ludwika Malinowska <malinowska.L@wp.pl>
2020-12-05 15:06:09 +0100

    AffineCipher - Deszyfrowanie - wyjatek: przekazanie do metody liczby (int) - Red - Assert Py

--- tests/AffineCipherTests/test_AffineCipherAssertPy.py
+++ tests/AffineCipherTests/test_AffineCipherAssertPy.py
@@ -49,6 +49,9 @@ class AffineTest(unittest.TestCase):
     def test_encode_exception2(self):
         assert_that(self.temp.affine_cipher).raises(ValueError).when_called_with(None, 1, 3)
 
+    def test_decipher_exception1(self):
+        assert_that(self.temp.affine_decipher).raises(ValueError).when_called_with(1, 1, 3)
+
     def tearDown(self):
         self.temp = None
 
