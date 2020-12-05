60df50ade71929eb6e81657766c2098b990b965c
Ludwika Malinowska <malinowska.L@wp.pl>
2020-12-05 17:43:28 +0100

    CaesarCipher - Deszyfrowanie - wyjatek: litera spoza tablicy, np. polskie znaki - Red - Assert Py

--- tests/CaesarCipherTests/test_CaesarCipherAssertPy.py
+++ tests/CaesarCipherTests/test_CaesarCipherAssertPy.py
@@ -88,6 +88,9 @@ class CaesarTest(unittest.TestCase):
     def test_decode_exception2(self):
         assert_that(self.temp.caesar_decipher).raises(ValueError).when_called_with(None)
 
+    def test_decode_exception3(self):
+        assert_that(self.temp.caesar_decipher).raises(ValueError).when_called_with("ą")
+
     def tearDown(self):
         self.temp = None
 
