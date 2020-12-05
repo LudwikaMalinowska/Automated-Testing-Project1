730189332dd3f8cc1e8b8c20f97d07c1ec715dfe
Ludwika Malinowska <malinowska.L@wp.pl>
2020-12-05 18:00:18 +0100

    AffineCipher - Szyfrowanie - wyjatek: litera spoza tablicy, np. polskie znaki - Red - Py Hamcrest

--- tests/AffineCipherTests/test_AffineCipherPyHamcrest.py
+++ tests/AffineCipherTests/test_AffineCipherPyHamcrest.py
@@ -50,6 +50,9 @@ class AffineTest(unittest.TestCase):
     def test_cipher_exception2(self):
         assert_that(calling(self.temp.affine_cipher).with_args(None, 1, 3), raises(ValueError))
 
+    def test_cipher_exception3(self):
+        assert_that(calling(self.temp.affine_cipher).with_args("ą", 1, 3), raises(ValueError))
+
     def test_decipher_exception1(self):
         assert_that(calling(self.temp.affine_decipher).with_args(1, 1, 3), raises(ValueError))
 
