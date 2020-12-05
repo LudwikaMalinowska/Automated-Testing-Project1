33eaabc999c92f06254befffbfd029b365e2f7ae
Ludwika Malinowska <malinowska.L@wp.pl>
2020-12-05 20:53:36 +0100

    AffineCipher - Szyfrowanie - wyjatek: parametr b musi byc typu int - Green

--- src/AffineCipher/AffineCipher.py
+++ src/AffineCipher/AffineCipher.py
@@ -34,7 +34,7 @@ class AffineCipher:
 
     def affine_cipher(self, text, a, b):
 
-        if not isinstance(text, str) or not isinstance(a, int):
+        if not isinstance(text, str) or not isinstance(a, int) or not isinstance(b, int):
             raise ValueError
 
         if a <= 0:
