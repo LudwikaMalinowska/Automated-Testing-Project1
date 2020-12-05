e99cd4c56085335e1aa304b6775443226176ee14
Ludwika Malinowska <malinowska.L@wp.pl>
2020-12-05 14:47:36 +0100

    CaesarCipher - Deszyfrowanie - wyjatek: przekazanie do metody liczby (int) - Red - Assert Py

--- tests/CaesarCipherTests/test_CaesarCipherAssertPy.py
+++ tests/CaesarCipherTests/test_CaesarCipherAssertPy.py
@@ -79,6 +79,9 @@ class CaesarTest(unittest.TestCase):
     def test_encode_exception2(self):
         assert_that(self.temp.caesar_cipher).raises(ValueError).when_called_with(None)
 
+    def test_decode_exception1(self):
+        assert_that(self.temp.caesar_decipher).raises(ValueError).when_called_with(1)
+
     def tearDown(self):
         self.temp = None
 
