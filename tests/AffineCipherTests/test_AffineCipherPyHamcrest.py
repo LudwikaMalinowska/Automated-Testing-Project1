95a8a2f75ee30bf10d8d7d00558783eb29c16304
Ludwika Malinowska <malinowska.L@wp.pl>
2020-12-05 18:18:56 +0100

    AffineCipher - Szyfrowanie - wyjatek: parametr a musi byc dodatni - Red - Py Hamcrest

--- tests/AffineCipherTests/test_AffineCipherPyHamcrest.py
+++ tests/AffineCipherTests/test_AffineCipherPyHamcrest.py
@@ -53,6 +53,9 @@ class AffineTest(unittest.TestCase):
     def test_cipher_exception3(self):
         assert_that(calling(self.temp.affine_cipher).with_args("ą", 1, 3), raises(ValueError))
 
+    def test_cipher_exception4(self):
+        assert_that(calling(self.temp.affine_cipher).with_args("abc", -1, 3), raises(ValueError))
+
     def test_decipher_exception1(self):
         assert_that(calling(self.temp.affine_decipher).with_args(1, 1, 3), raises(ValueError))
 
