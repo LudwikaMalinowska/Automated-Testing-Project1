18078481e3a5952832d0059c4d7221e7143f61dc
Ludwika Malinowska <malinowska.L@wp.pl>
2020-12-06 14:49:32 +0100

    Menu - klasa CipherAll - przeniesienie wszystkiego do klas i metod, uruchamianie projektu z pliku __main__

--- src/CipherAll/CipherAll.py
+++ src/CipherAll/CipherAll.py
@@ -2,63 +2,75 @@ from MorseCode.MorseCode import MorseCode
 from CaesarCipher.CaesarCipher import CaesarCipher
 from AffineCipher.AffineCipher import AffineCipher
 
+class CipherAll:
+    def print_morse(self):
+        print("1. Tekst na Alfabet Morsa")
+        print("2. Alfabet Morsa na tekst")
+        morse = MorseCode()
+        nr = input()
+        if nr == "1":
+            text = input("Podaj text: ")
+            morse_text = morse.morse_encode(text)
+            print(morse_text)
+        elif nr == "2":
+            morse_text = input("Podaj text: ")
+            text = morse.morse_decode(morse_text)
+            print(text)
+        else:
+            print("Podaj prawidłowy numer")
 
-print("Wybierz sposób szyfrowania: ")
-print("1. Alfabet Morsa")
-print("2. Szyfr Cezara")
-print("3. Szyfr Afiniczny")
-cipher_type = input()
+    def print_caesar(self):
+        print("1. Tekst na Szyfr Cezara")
+        print("2. Szyfr Cezara na tekst")
+        caesar = CaesarCipher()
+        nr = input()
+        if nr == "1":
+            text = input("Podaj text: ")
+            caesar_text = caesar.caesar_cipher(text)
+            print(caesar_text)
+        elif nr == "2":
+            caesar_text = input("Podaj text: ")
+            text = caesar.caesar_decipher(caesar_text)
+            print(text)
+        else:
+            print("Podaj prawidłowy numer")
 
-if cipher_type == "1":
-    print("1. Tekst na Alfabet Morsa")
-    print("2. Alfabet Morsa na tekst")
-    morse = MorseCode()
-    nr = input()
-    if nr == "1":
-        text = input("Podaj text: ")
-        morse_text = morse.morse_encode(text)
-        print(morse_text)
-    elif nr == "2":
-        morse_text = input("Podaj text: ")
-        text = morse.morse_decode(morse_text)
-        print(text)
-    else:
-        print("Podaj prawidłowy numer")
+    def print_affine(self):
+        print("1. Tekst na Szyfr Cezara")
+        print("2. Szyfr Cezara na tekst")
+        aff = AffineCipher()
+        nr = input()
+        if nr == "1":
+            text = input("Podaj text: ")
+            a = int(input("Podaj parametr a: "))
+            b = int(input("Podaj parametr b: "))
+            aff_text = aff.affine_cipher(text, a, b)
+            print(aff_text)
+        elif nr == "2":
+            aff_text = input("Podaj text: ")
+            a = int(input("Podaj parametr a: "))
+            b = int(input("Podaj parametr b: "))
+            text = aff.affine_decipher(aff_text, a, b)
+            print(text)
+        else:
+            print("Podaj prawidłowy numer")
 
-elif cipher_type == "2":
-    print("1. Tekst na Szyfr Cezara")
-    print("2. Szyfr Cezara na tekst")
-    caesar = CaesarCipher()
-    nr = input()
-    if nr == "1":
-        text = input("Podaj text: ")
-        caesar_text = caesar.caesar_cipher(text)
-        print(caesar_text)
-    elif nr == "2":
-        caesar_text = input("Podaj text: ")
-        text = caesar.caesar_decipher(caesar_text)
-        print(text)
-    else:
-        print("Podaj prawidłowy numer")
-elif cipher_type == "3":
-    print("1. Tekst na Szyfr Cezara")
-    print("2. Szyfr Cezara na tekst")
-    aff = AffineCipher()
-    nr = input()
-    if nr == "1":
-        text = input("Podaj text: ")
-        a = int(input("Podaj parametr a: "))
-        b = int(input("Podaj parametr b: "))
-        aff_text = aff.affine_cipher(text, a, b)
-        print(aff_text)
-    elif nr == "2":
-        aff_text = input("Podaj text: ")
-        a = int(input("Podaj parametr a: "))
-        b = int(input("Podaj parametr b: "))
-        text = aff.affine_decipher(aff_text, a, b)
-        print(text)
-    else:
-        print("Podaj prawidłowy numer")
+    def main(self):
+        while True:
+            print("Wybierz sposób szyfrowania: ")
+            print("1. Alfabet Morsa")
+            print("2. Szyfr Cezara")
+            print("3. Szyfr Afiniczny")
+            print("4. Zamknij program")
+            cipher_type = input()
 
-else:
-    print("Podaj prawidłowy numer")
+            if cipher_type == "1":
+                self.print_morse()
+            elif cipher_type == "2":
+                self.print_caesar()
+            elif cipher_type == "3":
+                self.print_affine()
+            elif cipher_type == "4":
+                break
+            else:
+                print("Podaj prawidłowy numer")
---
+++ src/__main__.py
@@ -0,0 +1,5 @@
+from CipherAll.CipherAll import CipherAll
+
+if __name__ == "__main__":
+    cipher_all = CipherAll()
+    cipher_all.main()
